16237de25e913c0babdffb04387da564dbe5d3d8
Bridger B <bridgerpjbell@gmail.com>
2022-08-24 16:24:57 -0700

    Main and hello1Function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
 using namespace std;
+
+int hello1Function(string name1);
 
 int main()
 {
+	string name1="Bridger"; 
+ 	hello1Function(name1);
 
-	cout << "Hello";
-return 0;
+	return 0;
 }
