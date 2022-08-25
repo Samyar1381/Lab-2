757ae51cf422b6ed04c589c07bf28f15898ca003
Bridger B <bridgerpjbell@gmail.com>
2022-08-24 19:57:16 -0700

    hello1 and main edits

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-// Author Your Name
+// Samyar Dastmalchi & Bridger Bell
 // Description: Hello World in C++
 //============================================================
-
 #include <iostream>
 using namespace std;
+
 int hello1Function(string name1);
 int hello2Function(string name2);
 
 int main()
 {
-string name1="your name1";
+string name1="Samyar";
 hello1Function(name1);
-string name2="your name2";
+string name2="Bridger";
 hello2Function(name2);
 return 0;
 }
